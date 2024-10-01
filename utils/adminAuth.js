51c40eb6472599a8b95cb124764735994b3517f3
vishnupriyatk1996@gmail.com <vishnupriyatk1996@gmail.com>
2024-10-01 16:47:20 +0530

    Fix CORS issue by removing trailing slash from allowed origin

--- utils/adminAuth.js
+++ utils/adminAuth.js
@@ -3,7 +3,7 @@ const { JWT_SECRET } = require('./config');
 const Admin = require('../models/admin');
 
 const auth = {
-    isAuthenticated: (req, res, next) => {
+    /*isAuthenticated: (req, res, next) => {
         try {
             const token = req.cookies.token;
             if (!token) {
@@ -21,7 +21,31 @@ const auth = {
         } catch (error) {
             res.status(500).json({ message: error.message });
         }
-    },
+    },*/
+    isAuthenticated: (req, res, next) => {
+        try {
+            const authHeader = req.headers.authorization;
+            if (!authHeader) {
+                return res.status(401).json({ message: 'Unauthorized' });
+            }
+    
+            const token = authHeader.split(" ")[1]; // Extract token after "Bearer"
+            if (!token) {
+                return res.status(401).json({ message: 'Unauthorized' });
+            }
+    
+            jwt.verify(token, process.env.JWT_SECRET, (error, admin) => {
+                if (error) {
+                    return res.status(403).json({ message: 'Invalid token' });
+                }
+                req.adminId = admin.id;  // Attach the admin ID to the request
+                req.admin = admin;  // Attach the admin data to the request
+                next();
+            });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    },    
     isAdmin: async (req, res, next) => {
         try {
             const { adminId } = req;
