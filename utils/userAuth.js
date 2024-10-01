6491b3ea3d8ce4a44c782a660758401f91dbd11f
vishnupriyatk1996@gmail.com <vishnupriyatk1996@gmail.com>
2024-10-01 16:36:15 +0530

    Fix CORS issue by removing trailing slash from allowed origin

--- utils/userAuth.js
+++ utils/userAuth.js
@@ -4,14 +4,38 @@ const User = require('../models/user');
 const BorrowTransaction = require('../models/borrowTransaction'); 
 
 const userAuth = {
-    isAuthenticated: (req, res, next) => {
+    /*isAuthenticated: (req, res, next) => {
         try {
             const token = req.cookies.token;
             if (!token) {
                 return res.status(401).json({ message: 'Unauthorized' });
             }
 
-            jwt.verify(token, JWT_SECRET, (error, user) => {
+            jwt.verify(token, process.env.JWT_SECRET, (error, user) => {
+                if (error) {
+                    return res.status(403).json({ message: 'Invalid token' });
+                }
+                req.userId = user.id;
+                req.user = user;
+                next();
+            });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    }*/
+    isAuthenticated: (req, res, next) => {
+        try {
+            const authHeader = req.headers.authorization;
+            if (!authHeader) {
+                return res.status(401).json({ message: 'Unauthorized' });
+            }
+    
+            const token = authHeader.split(" ")[1];  // Extract token after "Bearer"
+            if (!token) {
+                return res.status(401).json({ message: 'Unauthorized' });
+            }
+    
+            jwt.verify(token, process.env.JWT_SECRET, (error, user) => {
                 if (error) {
                     return res.status(403).json({ message: 'Invalid token' });
                 }
@@ -23,6 +47,7 @@ const userAuth = {
             res.status(500).json({ message: error.message });
         }
     }
+    
 };
 
 module.exports = userAuth;
