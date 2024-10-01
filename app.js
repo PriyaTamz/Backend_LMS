3d02c6db8eaec46bdb8ca4bd799845862bb358cf
vishnupriyatk1996@gmail.com <vishnupriyatk1996@gmail.com>
2024-10-01 14:59:38 +0530

    Fix CORS issue by removing trailing slash from allowed origin

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin: 'https://glittery-starburst-79895d.netlify.app/',
+    origin: 'https://glittery-starburst-79895d.netlify.app',
     credentials: true,
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
