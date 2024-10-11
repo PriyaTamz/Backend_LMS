0d5b45d475d7704f7ff7b1b3c8ca3e0f592220fa
vishnupriyatk1996@gmail.com <vishnupriyatk1996@gmail.com>
2024-10-11 16:11:13 +0530

    backend: Library management system

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://heroic-faloodeh-f3be09.netlify.app/',
     credentials: true,
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
