0f304ece3427480b74a95b4fa6f5a5e1a97f5981
PriyaTamz <vishnupriyatk1996@gmail.com>
2025-02-07 12:12:50 +0530

    re submission

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    //origin: 'https://heroic-faloodeh-f3be09.netlify.app',
-    origin: 'http://localhost:5173',
+    origin: 'https://heroic-faloodeh-f3be09.netlify.app',
+    //origin: 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
