2cf30ddb7cb2c57b12280d20e04f2efdbf1c301f
PriyaTamz <vishnupriyatk1996@gmail.com>
2025-02-09 20:42:06 +0530

    re submission

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const app = require('./app');
-
 const mongoose = require('mongoose');
 const { MONGODB_URI } = require('./utils/config');
 
+// Use the port provided by Render or fallback to 3001 locally
+const PORT = process.env.PORT || 3001;
+
+// Connect to MongoDB
 mongoose.connect(MONGODB_URI)
     .then(() => {
         console.log("Connected to the MongoDB database");
-        //console.log("MONGODB_URI:", process.env.MONGODB_URI);
 
-
-        app.listen(3001, () => {
-            console.log("Server is running on http://localhost:3001");
+        // Start the Express server on the dynamically assigned port
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
         });
     })
     .catch((err) => {
