b9b09e6fda8ea26962667c5892098616175ff09a
PriyaTamz <vishnupriyatk1996@gmail.com>
2025-02-09 20:32:46 +0530

    re submission

--- app.js
+++ app.js
@@ -9,9 +9,20 @@ const cors = require('cors');
 
 const app = express();
 
+// Dynamically handle allowed origins for CORS
+const allowedOrigins = [
+    'http://localhost:5173', // Local frontend
+    'https://heroic-faloodeh-f3be09.netlify.app' // Deployed frontend
+];
+
 app.use(cors({
-    origin: 'https://heroic-faloodeh-f3be09.netlify.app',
-    //origin: 'http://localhost:5173',
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true,
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
@@ -21,6 +32,7 @@ app.use(express.json());
 
 app.use(cookieParser());
 
+// Define routes
 app.use('/books', bookRouter);
 app.use('/admin', adminRouter);
 app.use('/user', userRouter);
